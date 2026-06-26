83d2e8628ff1466cdcb943a5ce99a91f60ac795c
Aryan214G <aryan214gm@gmail.com>
2026-06-26 15:46:39 +0530

    refactor: improve movie fetching logic in Home component

--- cine-stream/src/pages/Home.jsx
+++ cine-stream/src/pages/Home.jsx
@@ -1,17 +1,33 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { getPopularMovies } from '../services/tmdb';
 
 const Home = () => {
 
     const [movies, setMovies] = useState([]);
 
+    const fetchMovies = async () => {
+
+        const data = await getPopularMovies();
+        setMovies(data);
+    }
+
     useEffect(() => {
-        setMovies(getPopularMovies());
+        fetchMovies();
     }, []);
 
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div>
+
+            <div>
+
+                {
+                    movies.map(movie =>
+                        <p> {movie.title} </p>
+                    )
+                }
+            </div>
+        </div>
+    )
 }
 
 export default Home
